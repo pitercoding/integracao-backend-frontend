5fe64ae676d119ea56ad6588c5adf910f626b5fe
pitercoding <piterg.bio@gmail.com>
2025-09-24 10:19:46 +0200

    chore: initial commit with Spring Boot project setup

---
+++ src/test/java/com/pitergomes/integracao/IntegracaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pitergomes.integracao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegracaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

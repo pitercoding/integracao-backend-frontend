2665db7a1335c479a4940bd947a6830830b752c5
pitercoding <piterg.bio@gmail.com>
2025-09-24 10:33:43 +0200

    feat(controller): add APIController with GET /mensagem and POST /dados endpoints

---
+++ src/main/java/com/pitergomes/integracao/controller/APIController.java
@@ -0,0 +1,19 @@
+package com.pitergomes.integracao.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+
+public class APIController {
+
+    @GetMapping("/mensagem")
+    public String getMensagem() {
+        return "Dados do Back-End Spring Boot";
+    }
+
+    @PostMapping("/dados")
+    public String postMethodName (@RequestBody String dados){
+        return "Recebido: " + dados;
+    }
+}
